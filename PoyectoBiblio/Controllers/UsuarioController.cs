0763e2759cd496634ce5c4e8af604ebefad93b93
Androxito <francogamez23@gmail.com>
2023-08-15 14:03:07 -0700

    Agregar archivos de proyecto.

---
+++ PoyectoBiblio/Controllers/UsuarioController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PoyectoBiblio.Datos;
+using PoyectoBiblio.Models;
+namespace PoyectoBiblio.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        UsuarioDatos _usuariodatos = new UsuarioDatos();    
+        public IActionResult Listar()
+        {
+            var lista = _usuariodatos.Listar();
+            return View(lista);
+        }
+        public IActionResult Guardar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Guardar(UsuarioModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            var respuesta = _usuariodatos.GuardarUsuario(model);
+            if (respuesta)
+            {
+                return RedirectToAction("Listar");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult Editar(int IdUsuario)
+        {
+            UsuarioModel _usuario = _usuariodatos.ObtenerUsuario(IdUsuario);
+
+            return View(_usuario);
+
+            
+        }
+        [HttpPost]
+        public IActionResult Editar(UsuarioModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                return View();
+            }
+            var repuesta = _usuariodatos.EditarUsuario(model);
+            if(repuesta)
+            {
+                return RedirectToAction("Listar");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult Eliminar(int IdUsuario)
+        {
+            var _usuario = _usuariodatos.ObtenerUsuario(IdUsuario);
+            return View(_usuario);
+        }
+        [HttpPost]
+        public IActionResult Eliminar(UsuarioModel model)
+        {
+            var respuesta = _usuariodatos.EliminarUsuario(model.IdUsuario);
+            if (respuesta)
+            {
+                return RedirectToAction("Listar");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PoyectoBiblio/Datos/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+namespace PoyectoBiblio.Datos
+{
+    public class Conexion
+    {
+        private string cadenaSql = string.Empty;
+
+        public Conexion() 
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            cadenaSql = builder.GetSection("ConnectionStrings:cadenaSql").Value;
+        
+        
+        }
+        public string getCadenaSql()
+        {
+            return cadenaSql;
+        }
+
+
+    }
+}
